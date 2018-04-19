dd815c5c1bd4684f796400f407da9e6efb8d4816
Sujitha Vikram <sujithas875@gmail.com>
2018-04-18 20:00:43 -0700

    adding server spec file

---
+++ spec/db.spec.js
@@ -0,0 +1,37 @@
+const db = require('../db')
+describe('db test for creating and finding reviews', function() {
+    
+    it('should find reviews from db', function (done) {
+        // var sampleData = [{
+        //     locationId : 0,
+        //     customerName : 'From test 1', 
+        //     customerProfilePhotoUrl : 'From test 1',
+        //     customerReview : 'From test 1',
+        //     createdAt : new Date(),
+        //     ratingCleanliness : 1,
+        //     ratingCheckIn : 1,
+        //     ratingLocation : 1,
+        //     ratingValue : 1
+        // }]
+        // var callback = ((err, results) => {
+        //     if (err) {
+        //     console.log('db.test error ocured in saving to db-', err);
+        //     }
+        //     console.log('db.test results saving to db-', results);
+        //     expect(results[0]['customerName']).toBe('From test 1');
+        //     done();
+        // });
+
+        db.find(1, (err, result) => {
+            expect(result.length).toBeGreaterThan(0);
+            done();
+        });       
+    });
+
+    it('should save and find reviews from db', function (done) {
+        db.find(10001, (err, result) => {
+            expect(result.length).toBe(0);
+            done();
+        });
+    });
+});
---
+++ spec/server.spec.js
@@ -0,0 +1,27 @@
+//const app = require('../server');
+var request = require('request');
+
+describe('server', function() {
+    it('should respond to GET requests for /reviews with a 200 status code', function (done) {
+        request({method: 'GET',
+                uri : 'http://127.0.0.1:3000/reviews?locationId=1'},
+                function(error, response, body) {
+            expect(response.statusCode).toBe(200);
+            done();
+          });
+    });
+
+    it('Should 404 when asked for a nonexistent endpoint', function(done) {
+        request('http://127.0.0.1:3000/arglebargle', function(error, response, body) {
+          expect(response.statusCode).toBe(404);
+          done();
+        });
+      });
+    
+      it('Should 404 when asked when no locationId is provided', function(done) {
+        request('http://127.0.0.1:3000/reviews', function(error, response, body) {
+          expect(response.statusCode).toBe(404);
+          done();
+        });
+      });
+});
